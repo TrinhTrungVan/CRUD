b7f9dc1f3e1c26a7ef1de709733d34480a0a9eec
TrinhTrungVan <vantrinhtrung@gmail.com>
2024-01-03 13:41:58 +0700

    List products, Search, Delete product

---
+++ src/api/axiosClient.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const axiosClient = axios.create({
+  baseURL: 'https://64b7972921b9aa6eb0788b9f.mockapi.io/'
+})
+
+axiosClient.interceptors.request.use(async (config) => config)
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data
+    }
+    return response
+  },
+  (error) => {
+    return error.response
+    // throw error;
+  }
+)
+
+export default axiosClient
